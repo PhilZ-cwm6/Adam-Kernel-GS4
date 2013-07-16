0f1650c60152d903eccfa42f5ed92b327126d8ae
Mohamed Karami <karamimed@gmail.com>
2013-07-16 14:49:57 +0000

    repo cleanup

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 6 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 SMP PREEMPT Wed Jun 5 17:17:16 WEST 2013"
-#define LINUX_COMPILE_BY "wanam"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.4.5-Adam-1.3.3+"
